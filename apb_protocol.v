e0d66347b44a3bd4542a67c5540ac6d17315d646
vnarayana-455 <143677908+vnarayana-455@users.noreply.github.com>
2023-08-31 14:52:51 +0530

    Update apb_protocol.v

--- apb_protocol.v
+++ apb_protocol.v
@@ -1,60 +1,56 @@
 `timescale 1ns / 1ps
-module apb_protocol(clk,rst,pready,pread,penable,
-pwrite,addr,pwdata,psel,prdata);
-parameter S_IDLE = 3'b000;
-parameter S_SETUP = 3'b001;
-parameter S_ACCESS = 3'b010;
-input clk,rst,pready,pread,penable;
-input [7:0]pwrite;
-input [3:0]addr;
-output reg [7:0]pwdata;
-output reg psel;
-output reg [7:0]prdata;
-reg [2:0]state,nxt_state;
-reg [7:0] databus,addrbus;
-initial begin
-if(rst ==1)begin
-psel =0;end
-pwdata =0;prdata=0;
-end
-always@(posedge clk) begin
-    if(penable)begin
-        if(pready)begin
-            if(pwrite)begin
-            addrbus = addr;
-            databus = pwdata;       
-            end            
-            if(pread) begin
-            prdata = pwdata;
-            end
-        end
-    end
-    
+module apb1_protocol #(datawidth = 16,
+addresswidth = 8) (
+input clk,
+input rst_n,
+input [addresswidth-1:0]paddr,
+input [datawidth-1:0] pwdata,
+input pwrite,
+input psel,
+input penable,
+ output reg [datawidth-1:0]prdata
+);
+
+reg [datawidth-1:0] mem[31:0];
+
+reg [1:0] state;
+reg [1:0] IDLE = 0;
+reg [1:0] SETUP = 1;
+reg [1:0] WRITE = 2;
+reg [1:0] READ = 3;
+reg pready = 0;
+
+always @(posedge clk or negedge rst_n) begin
+ if(!rst_n) begin
+ prdata <= 0;
+ state <= SETUP; 
+ end else begin
+ case(state)
+ SETUP : begin
+ prdata <= 0;
+ if (psel & !penable) begin
+ if (pwrite) begin
+ state <= WRITE;
+ end else begin
+ state <= READ;
  end
-always@(nxt_state) begin
-    state = nxt_state;
+ end
+ end
+ WRITE: begin
+ if (psel & penable & pwrite) begin
+ mem[paddr] <= pwdata; 
+ end
+ state <= SETUP; 
+ end
+ READ : begin
+ if (psel & penable & !pwrite) begin
+ prdata <= mem[paddr];
+ end
+ state <= SETUP;
+ end
+ endcase
+ end
+
 end
-always@(posedge clk) begin
-case(state)
-    S_IDLE:begin
-    psel = 0;
-        if (pready == 1)  begin
-            nxt_state = S_SETUP;
-        end
-    end
-    S_SETUP:begin
-    psel = 1;
-    nxt_state = S_ACCESS;   
-    end
-    S_ACCESS:begin
-    psel = 1;
-        if (pready == 0)  begin
-            nxt_state = S_ACCESS;   
-        end
-        if(pready == 1) begin
-            nxt_state = S_SETUP;
-        end       
-    end
-endcase
-end             
+
 endmodule
